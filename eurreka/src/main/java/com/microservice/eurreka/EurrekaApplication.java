ff11864a242c22250cd6fc340c69f9a6ecd2eb19
lehuy16 <cwalklhuy@gmail.com>
2022-09-01 21:34:59 +0700

    [S1][Init] Init project

---
+++ eurreka/src/main/java/com/microservice/eurreka/EurrekaApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.eurreka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurrekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurrekaApplication.class, args);
+    }
+
+}
---
+++ eurreka/src/test/java/com/microservice/eurreka/EurrekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.eurreka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurrekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
